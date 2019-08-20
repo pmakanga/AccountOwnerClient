d11474268f41c7195a18b02b0f289915b2557790
Patrick <pmakanga@gmail.com>
2019-08-20 17:14:40 +0300

    Creating the Shared Module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
+import { SuccessModalComponent } from './modals/success-modal/success-modal.component';
+import { DatepickerDirective } from './directives/datepicker.directive';
+
+
+
+@NgModule({
+   declarations: [
+      ErrorModalComponent,
+      SuccessModalComponent
+   ],
+   imports: [
+      CommonModule,
+      DatepickerDirective
+   ],
+   exports: [
+      ErrorModalComponent,
+      SuccessModalComponent
+   ]
+})
+export class SharedModule { }
